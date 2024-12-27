46e98a9fe7f31c40774dfcfe19a1b5217925e378
deividas strole <deiwis@yahoo.com>
2024-12-27 10:16:55 -0800

    wip

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,18 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+
 const current0El = document.getElementById('score--0');
 const current1El = document.getElementById('score--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
+
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
@@ -39,5 +44,8 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active'); //remove or add
+    player1El.classList.toggle('player--active'); //remove or add
   }
 });
